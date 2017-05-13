3c1bbd3759d539137e61753f719a1e7b3e8cb2a1
Muzammil Hussain <cmuzammil786@gmail.com>
2017-05-13 10:44:50 +0530

    add validations

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
+
+
 var app = express();
 var PORT = process.env.PORT || 8080;
 var todos = [];
@@ -17,16 +20,17 @@ app.get('/todos', function (req, res){
 
 app.get('/todos/:id', function(req, res){
 	var todoId = parseInt(req.params.id, 10);
-	var x ;
-	todos.forEach(function (todo){
-		if(todoId === todo.id){
-			x = todo;
-		}
-	});		if (x) {
-			res.json(x)
-		}else{
-			res.status(404).send();
-		}
+	var x = _.findWhere(todos, {id: todoId});
+	// var x ;
+	// todos.forEach(function (todo){
+	// 	if(todoId === todo.id){
+	// 		x = todo;
+	// 	}
+	// });		if (x) {
+	// 		res.json(x)
+	// 	}else{
+	// 		res.status(404).send();
+	// 	}
 
 		if (x) {
 			res.json(x);
@@ -37,7 +41,13 @@ app.get('/todos/:id', function(req, res){
 });
 
 app.post('/todos', function(req, res){
-	var body = req.body;
+	var body = _.pick(req.body, 'description', 'completed');
+
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send();
+	}
+
+	body.description = body.description.trim();
 
 	body.id = todoNextid++;
 
