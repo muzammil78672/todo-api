2620253fead9be9c0509d9d132d069a3e09a20c0
Muzammil Hussain <cmuzammil786@gmail.com>
2017-05-15 15:50:38 +0530

    make changes in todos

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 
 
 var app = express();
@@ -70,17 +71,22 @@ app.get('/todos/:id', function(req, res){
 app.post('/todos', function(req, res){
 	var body = _.pick(req.body, 'description', 'completed');
 
-	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-		return res.status(400).send();
-	}
+	db.todo.create(body).then(function(todo){
+		res.json(todo.toJSON());
+	},function (e){
+		res.status(400).json(e);
+	});
+	// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+	// 	return res.status(400).send();
+	// }
 
-	body.description = body.description.trim();
+	// body.description = body.description.trim();
 
-	body.id = todoNextid++;
+	// body.id = todoNextid++;
 
-	todos.push(body);
+	// todos.push(body);
 
-	res.json(body);
+	// res.json(body);
 });
 
 //To delete todos by id
@@ -131,6 +137,11 @@ app.put('/todos/:id', function (req, res){
 
 });
 
-app.listen(PORT, function(){
+db.sequelize.sync().then(function(){
+	app.listen(PORT, function(){
 	console.log("express server started!");
-}); 
+});
+
+});
+
+ 
