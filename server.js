8c5047ccebee73701ef78cf3b8cdbc2eb800cc01
Muzammil Hussain <cmuzammil786@gmail.com>
2017-05-15 12:09:40 +0530

    final static todos

--- server.js
+++ server.js
@@ -20,8 +20,29 @@ app.get('/todos', function (req, res){
 	res.json(todos);
 });
 
+//to get /todos?completed=true
 
-//To gget todos by id 
+app.get('/todos', function(req, res){
+	var queryParams = req.query;
+	var filteredTodos = todos;
+	if (queryParams.hasOwnProperty('completed') && queryParams.completed == 'true') {
+		filteredTodos = _.where(filteredTodos, {completed: true});
+	}else if (queryParams.hasOwnProperty('completed') && queryParams.completed == 'false') {
+		filteredTodos = _.where(filteredTodos, {completed: false});
+	}
+
+	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+		filteredTodos = _.filter(filteredTodos, function(todo){
+			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+		});
+	}
+
+	res.json(filteredTodos);
+
+});
+
+
+//To get todos by id 
 app.get('/todos/:id', function(req, res){
 	var todoId = parseInt(req.params.id, 10);
 	var x = _.findWhere(todos, {id: todoId});
