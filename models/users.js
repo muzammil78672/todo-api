2217ae5817f3dd98ffda0bcadd43546d760117a3
Muzammil Hussain <cmuzammil786@gmail.com>
2017-05-17 12:20:52 +0530

    hash & salt password

--- models/users.js
+++ models/users.js
@@ -1,3 +1,6 @@
+var bcrypt = require('bcrypt');
+var _ = require('underscore');
+
 module.exports = function (sequelize, DataTypes){
 	return sequelize.define ('user', {
 	email: {
@@ -9,13 +12,27 @@ module.exports = function (sequelize, DataTypes){
 			notEmpty: false
 		}
 	},
+	salt: {
+		type: DataTypes.STRING
+	},
+	password_hash: {
+		type: DataTypes.STRING
+	},
 	password: {
-		type: DataTypes.STRING,
+		type: DataTypes.VIRTUAL,
 		allowNull: false,
 		validate: {
 				len: [7, 100],
 				notEmpty: false
-			}
+			},
+		set: function (value){
+			var salt = bcrypt.genSaltSync(10);
+			var hashPassword = bcrypt.hashSync(value, salt);
+
+			this.setDataValue('password', value);
+			this.setDataValue('salt', salt);
+			this.setDataValue('password_hash', hashPassword);
+		}
 	}
 },{
 	hooks: {
@@ -25,8 +42,16 @@ module.exports = function (sequelize, DataTypes){
 				user.email = user.email.toLowerCase();
 			}
 		}
+
+	},
+	instanceMethods: {
+		toPublicJSON: function(){
+			var json = this.toJSON();
+			return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
+		}
 	}
 
-	}
-	);
+
+
+	});
 };
