6ae1ba8451725b52a113a69db9619e11eba0516f
Muzammil Hussain <cmuzammil786@gmail.com>
2017-05-17 13:47:01 +0530

    add auth token generation

--- models/users.js
+++ models/users.js
@@ -1,5 +1,7 @@
 var bcrypt = require('bcrypt');
 var _ = require('underscore');
+var crypto = require('crypto-js');
+var jwt = require('jsonwebtoken');
 
 module.exports = function (sequelize, DataTypes){
 	var user =  sequelize.define ('user', {
@@ -74,6 +76,23 @@ module.exports = function (sequelize, DataTypes){
 		toPublicJSON: function(){
 			var json = this.toJSON();
 			return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
+		},
+		generateToken: function(type){
+			if (!_.isString(type)){
+				return undefined ;
+			}
+
+			try{
+
+				var stringData = JSON.stringify({id: this.get('id'), type: type});
+				var encryptedData = crypto.AES.encrypt(stringData, 'abc@123').toString();
+				var token = jwt.sign({
+					token: encryptedData
+				}, 'qwert123');
+				return token ;
+			}catch (e){
+				return undefined ;
+			}
 		}
 	}
 
